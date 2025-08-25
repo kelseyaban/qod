8a4af122f4058bf25769b7dbdf52891f44bf1592
Kelsey D. Aban <2022157114@ub.edu.bz>
2025-08-25 15:03:25 -0600

    Ecoded JSON v3

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,31 +1,20 @@
 package main
 
 import(
-	"encoding/json"
+	// "fmt"
 	"net/http"
-
 )
 
-func (a *application) healthcheckHandler (w http.ResponseWriter, r *http.Request) {
-	data := map[string] string {"status": "available", "environment": a.config.env, "version": a.config.vrs}
-
-	jsResponse, err := json.Marshal(data)
+func (app *application) healthcheckHandler (w  http.ResponseWriter, r *http.Request)  {
+	data := map[string]string {
+		"status": "available",
+		"environment": app.config.env,
+		"version": app.config.vrs,
+	  }
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-    	a.logger.Error(err.Error())
-    	http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-    	return
-	}
-
-	jsResponse = append(jsResponse, '\n')
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsResponse)
-
+		app.logger.Error(err.Error())
+			http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 }
 
-
-// func (app *application) healthcheckHandler (w  http.ResponseWriter, r *http.Request)  {
-// 	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
-// 	jsResponse = fmt.Sprintf(jsResponse, app.config.env, app.config.vrs)
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.Write([]byte(jsResponse))
-// }
+}
