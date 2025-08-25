365e770056ddc7a7bff20e0a698b934ee96268fa
Kelsey D. Aban <2022157114@ub.edu.bz>
2025-08-25 14:42:50 -0600

    Encoded JSON v2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,31 @@
 package main
 
 import(
-	"fmt"
+	"encoding/json"
 	"net/http"
+
 )
 
-func (app *application) healthcheckHandler (w  http.ResponseWriter, r *http.Request)  {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, app.config.vrs)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+func (a *application) healthcheckHandler (w http.ResponseWriter, r *http.Request) {
+	data := map[string] string {"status": "available", "environment": a.config.env, "version": a.config.vrs}
+
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+    	a.logger.Error(err.Error())
+    	http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+    	return
+	}
+
+	jsResponse = append(jsResponse, '\n')
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsResponse)
+
 }
+
+
+// func (app *application) healthcheckHandler (w  http.ResponseWriter, r *http.Request)  {
+// 	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
+// 	jsResponse = fmt.Sprintf(jsResponse, app.config.env, app.config.vrs)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write([]byte(jsResponse))
+// }
