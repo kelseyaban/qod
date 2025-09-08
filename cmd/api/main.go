1a974f7d2739e33fb6a1032760816db1d6428b66
Kelsey D. Aban <2022157114@ub.edu.bz>
2025-09-07 22:45:50 -0600

    connection pool setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,42 +3,56 @@
 package main
 
 import (
-	"fmt"
-    "flag"
-    "log/slog"
-    "os"
+	"flag"
+	"log/slog"
+	"os"
+	"time"
+	"context"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
-type configuration struct{
-    port int
-    env string
-    vrs string
-}
-//dependency injection
-type application struct{
-    config configuration
-    logger *slog.Logger
+type configuration struct {
+	port int
+	env  string
+	vrs  string
+	db   struct {
+		dsn string
+	}
 }
 
-func printUB() string {
-    return "Hello, UB!"
+// dependency injection
+type application struct {
+	config configuration
+	logger *slog.Logger
+	db *sql.DB
 }
+
+
 
 func main() {
-    greeting := printUB()
-    fmt.Println(greeting)
 
-    //Initialize configuration
-    cfg := loadConfig()
-    //Initialize logger
-    logger := setupLogger()
-    //Initialize applicatioin with dependencies
-    app := &application {
-        config: cfg,
-        logger: logger,
-    }
+	//Initialize configuration
+	cfg := loadConfig()
+	//Initialize logger
+	logger := setupLogger()
 
-    // Start the application server
+	//set up DB connection
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error("cannot connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	//Initialize applicatioin with dependencies
+	app := &application{
+		config: cfg,
+		logger: logger,
+		db: db,
+	}
+
+	// Start the application server
 	if err := app.serve(); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
@@ -47,20 +61,52 @@ func main() {
 }
 
 func loadConfig() configuration {
-    var cfg configuration
+	var cfg configuration
 
-    flag.IntVar(&cfg.port, "port", 4000, "API server port")
-    flag.StringVar(&cfg.env,"env", "development", "Environment(development|staging|production)")
-    flag.StringVar(&cfg.vrs,"version", "1.0.0", "Application version")
-    flag.Parse()
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.vrs, "version", "1.0.0", "Application version")
+	//read in the dsn
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://quotes:quotesadvweb@localhost/quotes", "PostgresSQL DSN")
+	flag.Parse()
 
-    return cfg
-} 
+	return cfg
+}
 
 func setupLogger() *slog.Logger {
-    var logger *slog.Logger
+	var logger *slog.Logger
 
-    logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-    return logger
+
+	logger.Info("database connection pool established")
+
+	return logger
+	
+}
+
+func openDB(cfg configuration) (*sql.DB, error) {
+	//open a connection pool
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(),5 * time.Second)
+
+	defer cancel()
+
+	//test if the connection pool was created
+	//try pinging it with a 5 second timeout
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	//return the connection pool (sql.DB)
+	return db, nil
+
 }
